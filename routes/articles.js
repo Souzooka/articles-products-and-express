9a6a70eeb1e76b0a0df198eab2662330e2aa12ad
Dakota <bazookaharwell@gmail.com>
2017-04-19 16:44:57 -1000

    Initial folder stup

---
+++ routes/articles.js
@@ -0,0 +1,15 @@
+var Articles = require('../db/articles.js');
+
+// returns the entire collection
+Articles.all();
+
+// adds a new article to the collection
+Articles.add({...});
+
+// returns the correct object from the collection
+Articles.getByTitle('The%20Best%20Magpie%20Developer%20of%202016');
+
+// finds an article in the collection by its title, if found - updates the article based on object passed as the second parameter then returns `true`
+// in the example below, it would change the title.
+// if the article is not found, returns `false`
+Articles.editByTitle('The%20Best%20Magpie%20Developer%20of%202016', { title: "..."});
