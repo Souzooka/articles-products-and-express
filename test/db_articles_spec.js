876fe370204750cf5cb0f563017b63bdedf5c02b
Dakota <bazookaharwell@gmail.com>
2017-04-20 08:48:29 -1000

    Tests -- More tests for db/articles.js add()

--- test/db_articles_spec.js
+++ test/db_articles_spec.js
@@ -28,8 +28,101 @@ describe('Articles Database', () => {
         'body'    :   'body'
       };
       articlesDB.add(article);
-      expect(articlesDB.all()).to.be.deep.equal([{'title':'title','author':'author','body':'body','urlTitle':'title'}]);
+      expect(articlesDB.all()).to.be.deep.equal([{
+        'title':'title',
+        'author':'author',
+        'body':'body',
+        'urlTitle':'title'
+      }]);
     });
 
+    it('should be able to add more than 1 article', () => {
+      let article = {
+        'title'   :   'title',
+        'author'  :   'author',
+        'body'    :   'body'
+      };
+      articlesDB.add(article);
+      article = {
+        'title'   :   'newTitle',
+        'author'  :   'author',
+        'body'    :   'body'
+      };
+      articlesDB.add(article);
+      expect(articlesDB.all()).to.be.deep.equal(
+      [{
+        'title':'newTitle',
+        'author':'author',
+        'body':'body',
+        'urlTitle':'newTitle'
+      },
+      {
+        'title':'title',
+        'author':'author',
+        'body':'body',
+        'urlTitle':'title'
+      }
+      ]);
+    });
+
+    it('should add articles and add a urlencoded urlTitle property', () => {
+      let article = {
+        'title'   :   'this is a title',
+        'author'  :   'author',
+        'body'    :   'body'
+      };
+      articlesDB.add(article);
+      expect(articlesDB.all()).to.be.deep.equal([
+      {
+        'title':'this is a title',
+        'author':'author',
+        'body':'body',
+        'urlTitle':'this%20is%20a%20title'
+      }
+      ]);
+    });
+
+    describe('validation', () => {
+
+      it('add() should return true if input is valid', () => {
+        let article = {
+          'title'   :   'title',
+          'author'  :   'author',
+          'body'    :   'body'
+        };
+        expect(articlesDB.add(article)).to.be.equal(true);
+      });
+
+      it('add() should return false if input is invalid', () => {
+        let article = {
+          'author'  :   'author',
+          'body'    :   'body'
+        };
+        expect(articlesDB.add(article)).to.be.equal(false);
+        article = {
+          'title'   :   'title',
+          'author'  :   'author',
+        };
+        expect(articlesDB.add(article)).to.be.equal(false);
+      });
+
+      it('add() should return false if given an article with a non-unique title', () => {
+        let article = {
+          'title'   :   'title',
+          'author'  :   'author',
+          'body'    :   'body'
+        };
+        expect(articlesDB.add(article)).to.be.equal(true);
+        expect(articlesDB.add(article)).to.be.equal(false);
+        expect(articlesDB.all()).to.be.deep.equal([
+        {
+        'title':'title',
+        'author':'author',
+        'body':'body',
+        'urlTitle':'title'
+        }
+        ]);
+      });
+    });
   });
 });
